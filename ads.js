a04df94aef8c74e2a61975da6b3236fd6055ce25
Tabib Imran <in.with.imran@gmail.com>
2025-06-21 19:05:51 +0600

    Update ads.js

--- ads.js
+++ ads.js
@@ -1,6 +1,6 @@
 ;(function(){
   
-	var icon = '<div class="sg"><img src="/imran.gif"></div>';
+	var icon = '<div class="sg"><img src="imran.gif"></div>';
 
 	document.head.insertAdjacentHTML('beforeend','<style>.sg img{ width: 60px; height: 60px; position: fixed; bottom: 15px; right: 25px; } .sg .eye { -webkit-transform: translateX(0px);   transform: translateX(0px); } .sg:hover .eye { -webkit-transition: -webkit-transform 0.3s ease; transition: -webkit-transform 0.3s ease; transition: transform 0.3s ease; transition: transform 0.3s ease, -webkit-transform 0.3s ease; -webkit-transform: translateX(12px);   transform: translateX(12px); }</style>'); 
   
